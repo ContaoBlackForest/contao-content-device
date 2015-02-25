7a279428e52f7746fc0f6f00b9e0962965a2dcc8
Sven Baumann <baumann.sv@gmail.com>
2015-02-25 09:32:11 +0100

    create it

---
+++ contao/config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Contao Content Device
+ * Copyright (C) 2015 Sven Baumann
+ *
+ * PHP version 5
+ *
+ * @package   contaoblackforest/contao-content-device
+ * @file      config.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright ContaoBlackforest 2015
+ */
